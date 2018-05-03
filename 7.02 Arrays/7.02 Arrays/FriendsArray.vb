9ce2d03f49c83d471a0a19b49c521c2945442fcf
Aditya Bhatia <30786285+abhatia25@users.noreply.github.com>
2018-05-03 18:40:19 -0400

    Added Friends Array

---
+++ 7.02 Arrays/7.02 Arrays/FriendsArray.vb
@@ -0,0 +1,32 @@
+﻿'Aditya Bhatia
+'Friends Array
+'5/3/2018
+Public Class frmFriendsArray
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim FriendArr(4) As String  'declare the array - size 5
+
+        'Loop to add strings from InputBoxes to the array
+        For i As Integer = 0 To FriendArr.Length - 1
+            FriendArr(i) = InputBox("Enter a friend's name.", "BFF")
+        Next i
+
+        'Sort the array, ascending
+        Array.Sort(FriendArr)
+
+        'Loop to add the values from the array to the label using 
+        'concatenation
+        For i As Integer = 0 To FriendArr.Length - 1
+            Me.lblDisplay.Text &= FriendArr(i) & vbCrLf
+        Next i
+
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        End
+    End Sub
+
+    Private Sub NextToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NextToolStripMenuItem.Click
+        Me.Hide()
+
+    End Sub
+End Class
---
+++ 7.02 Arrays/7.02 Arrays/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("7.02 Arrays")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP Inc.")>
+<Assembly: AssemblyProduct("7.02 Arrays")>
+<Assembly: AssemblyCopyright("Copyright © HP Inc. 2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("eef6bc28-44e6-42eb-962d-b78f8b529b8f")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
