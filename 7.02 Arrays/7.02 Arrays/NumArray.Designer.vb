8c2a6c3502d957c43639ffafea6cde1b473f527c
Aditya Bhatia <30786285+abhatia25@users.noreply.github.com>
2018-05-03 19:02:31 -0400

    Added Numbers Array

---
+++ 7.02 Arrays/7.02 Arrays/NumArray.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmNumArray
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.lblDisplayAvg = New System.Windows.Forms.Label()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Location = New System.Drawing.Point(12, 31)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(260, 23)
+        Me.btnDisplay.TabIndex = 0
+        Me.btnDisplay.Text = "Display the Average"
+        Me.btnDisplay.UseVisualStyleBackColor = True
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(284, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.ExitToolStripMenuItem.Text = "E&xit"
+        '
+        'lblDisplayAvg
+        '
+        Me.lblDisplayAvg.Location = New System.Drawing.Point(12, 57)
+        Me.lblDisplayAvg.Name = "lblDisplayAvg"
+        Me.lblDisplayAvg.Size = New System.Drawing.Size(260, 117)
+        Me.lblDisplayAvg.TabIndex = 2
+        Me.lblDisplayAvg.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'frmNumArray
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 184)
+        Me.Controls.Add(Me.lblDisplayAvg)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "frmNumArray"
+        Me.Text = "Numbers Array"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnDisplay As Button
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents lblDisplayAvg As Label
+End Class
---
+++ 7.02 Arrays/7.02 Arrays/NumArray.vb
@@ -0,0 +1,34 @@
+﻿'Aditya Bhatia
+'Numbers Array
+'5/3/2018
+Public Class frmNumArray
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+        Dim intNumArr(2) As Integer
+        Dim average As Double
+        addToArray(intNumArr)
+        average = getAverage(intNumArr)
+        Me.lblDisplayAvg.Text = "The average of the numbers is " &
+    average.ToString("##.0")
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        End
+    End Sub
+
+    'Function to determine and return the average given the array.
+    Function getAverage(ByVal intNumArr() As Integer) As Double
+        Dim sum, avg As Double
+        For Each num As Integer In intNumArr
+            sum += num
+        Next
+        avg = sum / intNumArr.Length
+        Return avg
+    End Function
+
+    'Sub to add values to the array
+    Private Sub addToArray(ByRef intNumArr() As Integer)
+        For i As Integer = 0 To intNumArr.Length - 1
+            intNumArr(i) = InputBox("Enter a Number")
+        Next i
+    End Sub
+End Class
