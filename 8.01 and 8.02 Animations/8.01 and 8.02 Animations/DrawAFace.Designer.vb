f04f02925730129b2303d880172ab07f14f09d00
Aditya Bhatia <30786285+abhatia25@users.noreply.github.com>
2018-05-14 17:51:53 -0400

    Added Draw A Face Animation

---
+++ 8.01 and 8.02 Animations/8.01 and 8.02 Animations/DrawAFace.Designer.vb
@@ -0,0 +1,100 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmDrawAFace
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.btnHappy = New System.Windows.Forms.Button()
+        Me.btnSad = New System.Windows.Forms.Button()
+        Me.picFrame = New System.Windows.Forms.PictureBox()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.picFrame, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(284, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.ExitToolStripMenuItem.Text = "E&xit"
+        '
+        'btnHappy
+        '
+        Me.btnHappy.Location = New System.Drawing.Point(12, 27)
+        Me.btnHappy.Name = "btnHappy"
+        Me.btnHappy.Size = New System.Drawing.Size(75, 23)
+        Me.btnHappy.TabIndex = 1
+        Me.btnHappy.Text = "Happy"
+        Me.btnHappy.UseVisualStyleBackColor = True
+        '
+        'btnSad
+        '
+        Me.btnSad.Location = New System.Drawing.Point(197, 27)
+        Me.btnSad.Name = "btnSad"
+        Me.btnSad.Size = New System.Drawing.Size(75, 23)
+        Me.btnSad.TabIndex = 2
+        Me.btnSad.Text = "Sad"
+        Me.btnSad.UseVisualStyleBackColor = True
+        '
+        'picFrame
+        '
+        Me.picFrame.Location = New System.Drawing.Point(12, 56)
+        Me.picFrame.Name = "picFrame"
+        Me.picFrame.Size = New System.Drawing.Size(260, 193)
+        Me.picFrame.TabIndex = 3
+        Me.picFrame.TabStop = False
+        '
+        'frmDrawAFace
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.picFrame)
+        Me.Controls.Add(Me.btnSad)
+        Me.Controls.Add(Me.btnHappy)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "frmDrawAFace"
+        Me.Text = "Draw A Face"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.picFrame, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents btnHappy As Button
+    Friend WithEvents btnSad As Button
+    Friend WithEvents picFrame As PictureBox
+End Class
---
+++ 8.01 and 8.02 Animations/8.01 and 8.02 Animations/DrawAFace.vb
@@ -0,0 +1,68 @@
+﻿'Aditya Bhatia
+'Draw A Face
+'5/14/2018
+Public Class frmDrawAFace
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        End
+    End Sub
+
+    Private Sub btnHappy_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnHappy.Click
+        Dim pictureBoxSurface As Graphics = Me.picFrame.CreateGraphics
+        pictureBoxSurface.Clear(Me.BackColor)
+        Dim maxX As Integer = Me.picFrame.Size.Width
+        Dim maxY As Integer = Me.picFrame.Size.Height
+
+        'Define all the brushes
+        Dim tanBrush As New SolidBrush(Color.Tan)
+        Dim grayBrush As New SolidBrush(Color.LightGray)
+        Dim blueBrush As New SolidBrush(Color.Blue)
+        Dim redBrush As New SolidBrush(Color.Red)
+        Dim pinkBrush As New SolidBrush(Color.PaleVioletRed)
+
+        'Draw rectangle in background of picturebox
+        pictureBoxSurface.FillRectangle(tanBrush, 0, 0, maxX, maxY)
+
+        'Draw large ellipse for face
+        pictureBoxSurface.FillEllipse(grayBrush, 1, 1, maxX - 3, maxY - 3)
+
+        'Draw eyes
+        pictureBoxSurface.FillEllipse(blueBrush, maxX \ 3 - 10, 30, 35, 35)
+        pictureBoxSurface.FillEllipse(blueBrush, maxX \ 2 + 10, 30, 35, 35)
+
+        'Draw nose
+        pictureBoxSurface.FillEllipse(redBrush, maxX \ 2 - 10, maxY \ 2, 20, 20)
+
+        'Draw mouth
+        pictureBoxSurface.FillPie(pinkBrush, maxX \ 3 - 5, maxY \ 4 + 40, 80, 80, 0, 180)
+    End Sub
+
+    Private Sub btnSad_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSad.Click
+        Dim pictureBoxSurface As Graphics = Me.picFrame.CreateGraphics
+        pictureBoxSurface.Clear(Me.BackColor)
+        Dim maxX As Integer = Me.picFrame.Size.Width
+        Dim maxY As Integer = Me.picFrame.Size.Height
+
+        'Define all the brushes
+        Dim tanBrush As New SolidBrush(Color.Tan)
+        Dim grayBrush As New SolidBrush(Color.LightGray)
+        Dim blueBrush As New SolidBrush(Color.Blue)
+        Dim redBrush As New SolidBrush(Color.Red)
+        Dim pinkBrush As New SolidBrush(Color.PaleVioletRed)
+
+        'Draw rectangle in background of picturebox
+        pictureBoxSurface.FillRectangle(tanBrush, 0, 0, maxX, maxY)
+
+        'Draw large ellipse for face
+        pictureBoxSurface.FillEllipse(grayBrush, 1, 1, maxX - 3, maxY - 3)
+
+        'Draw eyes
+        pictureBoxSurface.FillEllipse(blueBrush, maxX \ 3 - 10, 30, 35, 35)
+        pictureBoxSurface.FillEllipse(blueBrush, maxX \ 2 + 10, 30, 35, 35)
+
+        'Draw nose
+        pictureBoxSurface.FillEllipse(redBrush, maxX \ 2 - 10, maxY \ 2, 20, 20)
+
+        'Draw mouth
+        pictureBoxSurface.FillPie(pinkBrush, maxX \ 3 - 5, maxY \ 3 + 60, 80, 80, 0, -180)
+    End Sub
+End Class
